b42460a6e65c624460f73d6b31f15dd9689ab56d
katyastan <134015338+katyastan@users.noreply.github.com>
2024-02-10 15:15:08 +0100

    test

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -495,7 +495,8 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(nums) {
+function findLongestIncreasingSubsequence(/* nums */) {
+  throw new Error('Not implemented');
 }
 
 /**
